668aa7cedfebabd0bbccee335831590058f1e3e9
jonaselan <jonas.elan@gmail.com>
2019-09-24 22:31:30 -0300

    Create endpoints for store and list posts

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const PostController = require('./controllers/PostController')
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
 
 const routes = new express.Router();
+const upload = multer(uploadConfig);
 
+const PREFIX = '/api/v1';
 // every function the receive req and res as a parameter
 // MIDDLEWARE
-routes.get('/', (req, res) => {
-  return res.send('sim');
-});
+routes.post(`${PREFIX}/posts`, upload.single('image'), PostController.store);
+routes.get(`${PREFIX}/posts`, PostController.index);
 
 module.exports = routes;
