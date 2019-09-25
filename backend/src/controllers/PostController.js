79bcb344ae636592931ede31a225766fd9557e59
jonaselan <jonas.elan@gmail.com>
2019-09-24 23:06:31 -0300

    Resize images

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,4 +1,7 @@
 const Post = require('../models/Post');
+const path = require('path');
+const fs = require('fs');
+const sharp = require('sharp');
 
 module.exports = {
   async index(req, res) {
@@ -10,6 +13,16 @@ module.exports = {
   async store(req, res) {
     const { author, place, description, hashtags } = req.body;
     const { filename: image } = req.file;
+
+    // return res.json(req.file);
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70})
+      .toFile(
+        path.resolve(req.file.destination, 'resized', image)
+      )
+    // delete original image
+    fs.unlinkSync(req.file.path);
 
     const post = await Post.create({
       author, place, description, hashtags, image,
