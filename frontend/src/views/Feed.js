4d1cf3b06b5aa53f0adafc4d7135a8ce6d79ed77
jonaselan <jonas.elan@gmail.com>
2019-09-26 21:43:03 -0300

    Create base for feed

--- frontend/src/views/Feed.js
+++ frontend/src/views/Feed.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 
+import '../stylesheets/feed.css'
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
 class Feed extends Component {
   render() { // required
     return (
-      <h1> Feed </h1>
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Jonas</span>
+              <span className="place"> JC</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+
+          <img src="http://localhost:8000/files/template.png"/>
+
+          <footer>
+            <div className="actions">
+              <img src={like} alt="" />
+              <img src={comment} alt="" />
+              <img src={send} alt="" />
+            </div>
+
+            <strong>curtidas</strong>
+
+            <p>
+              akjsdh
+              <span> #sim </span>
+            </p>
+          </footer>
+        </article>
+      </section>
     );
   }
 }
